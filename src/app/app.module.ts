eb727dd9334418973958057d6d3f0c9a05bab360
softpager <deprogramminglord@gmail.com>
2022-02-03 09:35:21 +0700

    Project structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {UserComponent} from './components/user/user.component';
+import {RegistrationComponent} from './components/registration/registration.component';
+import {ResourcesComponent} from './components/resources/resources.component';
+import {SecurityComponent} from './components/security/security.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RegistrationComponent,
+    ResourcesComponent,
+    SecurityComponent
   ],
   imports: [
     BrowserModule
